194abb2b07761236791e01347395a1ca622bd02f
Luciano Ramello <luchosr@gmail.com>
2024-08-15 16:29:16 +0200

    Updates Meal Page component HTML structure, adds suspense  with  fallback

--- 05-onwards-foodies-starting-project/app/meals/page.js
+++ 05-onwards-foodies-starting-project/app/meals/page.js
@@ -1,27 +1,35 @@
 import Link from 'next/link';
+import { Suspense } from 'react';
+
 import classes from './page.module.css';
 import MealsGrid from '@/components/meals/meals-grid';
 import { getMeals } from '@/lib/meals';
+import MealsLoadingPage from './loading-out';
 
-const MealsPage = async () => {
+async function Meals() {
   const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
 
+const MealsPage = () => {
   return (
     <>
       <header className={classes.header}>
         <h1>
-          Delicitious meals, created{' '}
+          Delicious meals, created{' '}
           <span className={classes.highlight}>by you</span>
         </h1>
         <p>
-          Chosse your favorite recipe and cook it yourself. It is easy and fun!
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
         </p>
         <p className={classes.cta}>
           <Link href="/meals/share">Share Your Favorite Recipe</Link>
         </p>
       </header>
       <main className={classes.header}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<MealsLoadingPage />}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
