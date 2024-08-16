7d6ba3c64d3d3ea7e2f6a451de0d029d10e4d5b2
Luciano Ramello <luchosr@gmail.com>
2024-08-16 13:30:48 +0200

    Improve meals page metadata

--- 05-onwards-foodies-starting-project/app/meals/page.js
+++ 05-onwards-foodies-starting-project/app/meals/page.js
@@ -6,6 +6,11 @@ import MealsGrid from '@/components/meals/meals-grid';
 import { getMeals } from '@/lib/meals';
 import MealsLoadingPage from './loading-out';
 
+export const metadata = {
+  title: 'All Meals',
+  description: 'Browse the delicious meals shared by our  community.',
+};
+
 async function Meals() {
   const meals = await getMeals();
   return <MealsGrid meals={meals} />;
