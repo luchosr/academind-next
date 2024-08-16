1780cc36af530b87eb910e67c072ee6363fa05cf
Luciano Ramello <luchosr@gmail.com>
2024-08-16 13:31:27 +0200

    Improves text validation

--- 05-onwards-foodies-starting-project/lib/actions.js
+++ 05-onwards-foodies-starting-project/lib/actions.js
@@ -1,8 +1,14 @@
 'use server';
 
+import { revalidatePath } from 'next/cache';
 import { saveMeal } from './meals';
+import { redirect } from 'next/dist/server/api-utils';
 
-export const shareMeal = async (formData) => {
+function isInvalidText(text) {
+  return !text || text.trim() === '';
+}
+
+export const shareMeal = async (prevState, formData) => {
   const meal = {
     title: formData.get('title'),
     summary: formData.get('summary'),
@@ -12,5 +18,22 @@ export const shareMeal = async (formData) => {
     creator_email: formData.get('email'),
   };
 
+  if (
+    isInvalidText(meal.title) ||
+    isInvalidText(meal.summary) ||
+    isInvalidText(meal.instructions) ||
+    isInvalidText(meal.creator) ||
+    isInvalidText(meal.creator_email) ||
+    !meal.creator_email.includes('@') ||
+    !meal.image ||
+    meal.image.size === 0
+  ) {
+    return {
+      message: 'Invalid input',
+    };
+  }
+
   await saveMeal(meal);
+  revalidatePath('/meals');
+  redirect('/meals');
 };
