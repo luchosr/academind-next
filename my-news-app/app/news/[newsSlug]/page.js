6714db52fe8ae3aa79e6f918d0449f815a3fabb3
Luciano Ramello <luchosr@gmail.com>
2024-08-19 12:28:26 +0200

    feat/Implement news detail page

--- my-news-app/app/news/[newsSlug]/page.js
+++ my-news-app/app/news/[newsSlug]/page.js
@@ -1,11 +1,20 @@
-const News = ({ params }) => {
-  const newsId = params.newsSlug;
+import { DUMMY_NEWS } from '@/app/dummy-news';
+import Image from 'next/image';
+
+const NewsDetailPage = ({ params }) => {
+  const newsSlug = params.newsSlug;
+  const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsSlug);
+
   return (
-    <article>
-      <h1>The news is {params.newsSlug}</h1>
-      <p>News ID: {newsId}</p>
+    <article className="news-article">
+      <header>
+        <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        <h1>The news is {newsItem.title}</h1>
+        <time datetime={newsItem.date}>{newsItem.date}</time>
+      </header>
+      <p>{newsItem.content}</p>
     </article>
   );
 };
 
-export default News;
+export default NewsDetailPage;
