6c729e687892d01327c4c7bddd7643d009b05183
Luciano Ramello <luchosr@gmail.com>
2024-08-19 11:52:29 +0200

    Improves news page items

--- my-news-app/app/news/page.js
+++ my-news-app/app/news/page.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { DUMMY_NEWS } from '../dummy-news';
 
 const NewsPage = () => {
   return (
@@ -6,15 +7,17 @@ const NewsPage = () => {
       <h1>NewsPage</h1>
 
       <ul className="news-list">
-        <li>
-          <Link href={'/news/first-news'}>First News Item</Link>
-        </li>
-        <li>
-          <Link href={'/news/second-news'}>Second News Item</Link>
-        </li>
-        <li>
-          <Link href={'/news/third-news'}>Third News Item</Link>
-        </li>
+        {DUMMY_NEWS.map((newsItem) => (
+          <li key={newsItem.id}>
+            <Link href={`/news/${newsItem.slug}`}>
+              <img
+                src={`/images/news/${newsItem.image}`}
+                alt={newsItem.title}
+              />
+              <span>{newsItem.title}</span>
+            </Link>
+          </li>
+        ))}
       </ul>
     </main>
   );
